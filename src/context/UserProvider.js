9278fac5018dbd676ffc72c43a50cf2c2921b08f
devminchan <minchan.dev@gmail.com>
2020-06-21 02:07:48 +0900

    UserProvider 추가

---
+++ src/context/UserProvider.js
@@ -0,0 +1,20 @@
+import { useState, createContext } from 'react';
+
+export const UserContext = createContext({});
+
+function UserProvider(props) {
+  // eslint-disable-next-line react/prop-types
+  const { children } = props;
+  const [user, setUser] = useState({});
+
+  const value = {
+    userState: user,
+    setUserState: (userInfo) => {
+      setUser(userInfo);
+    },
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
+
+export default UserProvider;
