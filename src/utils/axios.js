54892998079bd4757a733f524ea7d3fa3b701346
devminchan <minchan.dev@gmail.com>
2020-06-21 00:02:56 +0900

    Custom axios instance 추가

---
+++ src/utils/axios.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const TOKEN_NAME = 'token';
+export const SERVER_URL =
+  'http://cute-cat-server-elb-1523823705.ap-northeast-2.elb.amazonaws.com';
+
+const token = localStorage.getItem(TOKEN_NAME);
+
+export default axios.create({
+  baseURL: SERVER_URL,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
