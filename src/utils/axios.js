988f2b4660d1ea4c9c32ade775e77d38f9d111bb
devminchan <minchan.dev@gmail.com>
2020-06-22 10:03:22 +0900

    SERVER_URL 변경

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export const TOKEN_NAME = 'token';
-export const SERVER_URL =
-  'http://cute-cat-server-elb-1523823705.ap-northeast-2.elb.amazonaws.com';
+export const SERVER_URL = 'https://api.my-cute-cat.com';
 
 const token = localStorage.getItem(TOKEN_NAME);
 
